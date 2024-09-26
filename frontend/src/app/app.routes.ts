1eb9d3ef8234836a2bb6889ea9782d39c4144571
lfs0412 <leon.f.s@outlook.com>
2024-09-26 15:34:02 +0200

    sidebar and routing works nice

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent} from "./login/login.component";
-import { DashboardComponent} from "./dashboard/dashboard.component";
+import { HomeComponent} from "./home/home.component";
 import { AuthGuard} from "./auth/auth.guard";
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
