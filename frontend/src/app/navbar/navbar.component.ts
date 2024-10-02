21475429fb802df476cec98269128009949e2dee
lfs0412 <leon.f.s@outlook.com>
2024-10-02 14:19:53 +0200

    styled breadcrumb + navbar

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
+import {User} from "../auth/user.interface";
+import {AuthService} from "../auth/auth.service";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
-  imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css'],
+  imports: [
+    RouterLinkActive,
+    NgClass,
+    RouterLink
+  ],
+  standalone: true
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  user: User | undefined;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    this.authService.getUserProfile().subscribe({
+      next: (data: User) => {
+        this.user = data;
+      },
+      error: (err) => {
+        console.error('Error fetching user profile', err);
+      }
+    });
+  }
+
+  isProjectTabActive(): boolean {
+    const currentUrl = this.router.url; // Get the current route URL
+    return (
+      currentUrl.startsWith('/projects') ||
+      currentUrl.startsWith('/entries') ||
+      currentUrl.startsWith('/chat')
+    );
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+
+  }
 }
